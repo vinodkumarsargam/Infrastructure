6ad572f0d75cac948f49a0879cc4822193a78c3b
vinodkumar sargam <vinaysargam7@gmail.com>
2026-04-28 16:37:38 +0530

    Change lockfile configuration to use DynamoDB

--- 1-network/backend.tf
+++ 1-network/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "vinodkumarsargam77777-infra-statefile-backup"
     key            = "vinodkumarsargam77777/1-network/terraform.tfstate"
     region         = "ap-south-1"
-    use_lockfile   = "vinodkumarsargam77777-terraform-locks"
+    dynamodb_table = "vinodkumarsargam77777-terraform-locks"
     encrypt        = true
   }
 }
