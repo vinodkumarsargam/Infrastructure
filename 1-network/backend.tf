3aceeaecd836d9f3a0ededb35aace4e12848eca2
vinodkumar sargam <vinaysargam7@gmail.com>
2026-04-28 17:14:10 +0530

    Update backend.tf

--- 1-network/backend.tf
+++ 1-network/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "vinodkumarsargam77777-infra-statefile-backup"
-    key            = "vinodkumarsargam77777/1-network/terraform.tfstate"
+    key            = "vinodkumarsargam77777-infra-statefile-backup/1-network/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "vinodkumarsargam77777-terraform-locks"
     encrypt        = true
