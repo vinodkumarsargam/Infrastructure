93615f76f566014f5ad19905192358d297482dc6
vinodkumar sargam <vinaysargam7@gmail.com>
2026-04-28 16:44:33 +0530

    Update main.tf

--- 0-bootstrap/main.tf
+++ 0-bootstrap/main.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket_public_access_block" "tf_state" {
 }
 
 resource "aws_dynamodb_table" "tf_lock" {
-  name         = "quantamvector-terraform-locks"
+  name         = "vinodkumarsargam77777-terraform-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
